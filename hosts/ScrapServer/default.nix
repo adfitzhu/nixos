3590192edc01273d1d1849825d7077cdac77d779
Adam Fitzhugh <adfitzhu@fitzworks.net>
2026-08-02 17:14:10 -0700

    added sunshine to scrapserver

--- hosts/ScrapServer/default.nix
+++ hosts/ScrapServer/default.nix
@@ -10,6 +10,42 @@
 
   networking.hostName = "ScrapServer";
 
+  services.sunshine = {
+    enable = true;
+    capSysAdmin = true;
+    openFirewall = true;
+    settings = {
+      channels = 2;
+      # encoder = "quicksync";  # Intel QuickSync
+      # encoder = "vaapi";      # AMD/Intel VA-API
+      # encoder = "nvenc";      # NVIDIA
+      bitrate = 30000;
+      fps = 60;
+      hevc_mode = 2;
+      av1_mode = 0;
+      min_log_level = 2;
+      qp = 28;
+      crf = 0;
+      rc = "cbr";
+      fec_percentage = 5;
+      min_threads = 2;
+    };
+    applications = {
+      env = {
+        PATH = "$(PATH):$(HOME)/.local/bin:/run/current-system/sw/bin";
+        DISPLAY = ":0";
+        WAYLAND_DISPLAY = "wayland-0";
+      };
+      apps = [
+        {
+          name = "Desktop";
+          exclude-global-prep-cmd = "false";
+          auto-detach = "true";
+        }
+      ];
+    };
+  };
+
   services.desktopManager.plasma6.enable = true;
   services.displayManager = {
     sddm.enable = true;
