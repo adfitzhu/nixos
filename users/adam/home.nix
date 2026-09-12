d279620bfd3cf749cdb6ece5e3f3f6e97de27bc3
Adam Fitzhugh <adfitzhu@fitzworks.net>
2026-09-11 21:31:05 -0700

    another home manager fix attempt

--- users/adam/home.nix
+++ users/adam/home.nix
@@ -9,13 +9,21 @@ in
   nixpkgs.config.allowUnfree = true;
   home.stateVersion = "25.05";
 
-  home.file.".local/share/flatpak/overrides/global" = {
-    force = true;
-    text = ''
-      [Context]
-      filesystems=home;/run/user/1000:ro
-    '';
-  };
+  home.activation.flatpakRuntimeOverride = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    override_dir="$HOME/.local/share/flatpak/overrides"
+    override_file="$override_dir/global"
+    override_tmp="$override_dir/global.hm-tmp"
+
+    run mkdir -p $VERBOSE_ARG "$override_dir"
+
+    if [[ ! -v DRY_RUN ]]; then
+      cat > "$override_tmp" <<EOF
+[Context]
+filesystems=home;/run/user/$UID:ro
+EOF
+      mv "$override_tmp" "$override_file"
+    fi
+  '';
 
  # services.nextcloud-client.enable = true;
 
